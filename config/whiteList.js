a1280e71953ddc82232b15effe41970564efa9fa
RolandOroz <rolandoroz@gmail.com>
2023-05-28 13:34:21 +0200

    adding exception to whitelist

--- config/whiteList.js
+++ config/whiteList.js
@@ -3,6 +3,8 @@ const whiteList = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3500/refresh",
+  "http://localhost:3500/register",
+  "http://localhost:3000",
 ];
 
 module.exports = whiteList 
