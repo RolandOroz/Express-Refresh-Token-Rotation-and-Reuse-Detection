e6d449c6cdab6ed3097f311b6b8343fe48ebd922
RolandOroz <rolandoroz@gmail.com>
2023-05-28 20:24:35 +0200

    testing_1

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,11 +1,11 @@
 const whiteList = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
   "http://localhost:3500",
-  "http://localhost:3500/refresh",
+  //"http://localhost:3500/refresh",
   "http://localhost:3500/register",
-  "http://localhost:3000",
+  //"http://127.0.0.1:3000/",
+  //"http://127.0.0.1:3000/register",
   "http://localhost:3000/register",
+  "http://localhost:3000"
 ];
 
 module.exports = whiteList 
